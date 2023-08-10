4f4cf1dc55b040087f2c275e42afe07edb7d18b5
VitalinaZ <126213780+VitalinaZ@users.noreply.github.com>
2023-08-10 17:02:00 +0000

    Deploying to gh-pages from @ VitalinaZ/goit-js-hw-09@c0df9117ccd8b21dbd8bb0678a473234907a0094 🚀

--- 01-color-switcher.794b4526.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,e.addEventListener("click",(function(){timeId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),e.disabled=!0,d.disabled=!1}),1e3)})),d.addEventListener("click",(function(){clearInterval(timeId),d.disabled=!0,e.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.794b4526.js.map
